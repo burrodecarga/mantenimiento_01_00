549161131040a9dae0dbee0b359c977a2d278642
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2022-12-07 20:43:32 -0400

    leccion 181

--- app/Http/Livewire/Mant/Timelines/TimelineReplacement.php
+++ app/Http/Livewire/Mant/Timelines/TimelineReplacement.php
@@ -2,12 +2,58 @@
 
 namespace App\Http\Livewire\Mant\Timelines;
 
+use App\Models\Replacement;
+use App\Models\Timeline;
 use Livewire\Component;
 
 class TimelineReplacement extends Component
 {
+    public $timeline, $timelineReplacements, $replacements;
+    public $replacementId, $quantity;
+
+    protected $rules=[
+        'replacementId'=>'required',
+        'quantity'=>'required|numeric|regex:/^[\d]{0,11}(\.[\d]{1,2})?$/',
+    ];
+
+    public function mount(Timeline $timeline)
+    {   $this->timeline = $timeline;
+        $this->timelineRemplacements = $timeline->replacements;
+        //dd($this->timelineRemplacements);
+    }
+
+    public function saveReplacement(){
+        $this->validate();
+
+        $this->replacement = Replacement::find($this->replacementId);
+        $price = $this->replacement->price;
+        $total = $this->replacement->price * $this->quantity;
+        $quantity = $this->quantity;
+       $this->replacement->timelines()->attach($this->timeline->id,[
+            'price'=>$price,
+            'quantity'=>$quantity,
+            'total'=>$total
+        ]);
+
+        $this->reset('quantity','replacementId');
+        $this->timelineReplacements = $this->timeline->replacements;
+
+        $this->dispatchBrowserEvent('swal', [
+            'title' => 'Accion ejecutada',
+            'timer'=>1000,
+            'icon'=>'success',
+            'toast'=>true,
+            'position'=>'top-right'
+        ]);
+
+
+ $this->emitTo('mant.timelines.timeline-replacement-list','replacemetadded');
+    }
+
     public function render()
     {
+$this->replacements = Replacement::orderBy('name')->get();
+
         return view('livewire.mant.timelines.timeline-replacement');
     }
 }
--- app/Http/Livewire/Mant/Timelines/TimelineReplacementList.php
+++ app/Http/Livewire/Mant/Timelines/TimelineReplacementList.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\Mant\Timelines;
 
+use App\Models\Timeline;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class TimelineReplacementList extends Component
 {
+    public $timeline, $replacements;
+
+    protected $listeners = ['replacemetadded' => 'actualizar'];
+
+    public function actualizar(){
+        $this->timeline->load('replacements');
+     $this->render();
+    }
+
+    public function mount(Timeline $timeline)
+    {   $this->timeline = $timeline;
+        $this->replacements = $timeline->replacements;
+    }
+
+    public function remove($rfId){
+     DB::table('timeline_replacement')->where('id',$rfId)->delete();
+     $this->timeline->load('replacements');
+     $this->render();
+    }
+
+
     public function render()
     {
+        $this->replacements = $this->timeline->replacements;
         return view('livewire.mant.timelines.timeline-replacement-list');
     }
 }

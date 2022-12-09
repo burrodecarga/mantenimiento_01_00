3779ebd154c39af9b691444bb4563d9709ebe754
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2022-12-09 13:28:26 -0400

    leccion 187-error

--- app/Services/DatosService.php
+++ app/Services/DatosService.php
@@ -1,11 +1,24 @@
 <?php namespace App\Services;
 
 use App\Interfaces\DatosServiceInterface;
+use App\Models\Fail;
+use App\Models\Timeline;
+use Illuminate\Support\Facades\DB;
 
 class DatosService implements DatosServiceInterface
 {
     public function prueba()
     {
         echo "Funciona correctamente";
+    }
+
+    public function failCost(){
+
+    $timelines = Timeline::withSum('replacements','replacement_timeline.total')
+                         ->withSum('supplies','supply_timeline.total')
+                         ->withSum('services','service_timeline.price')
+                         ->withSum('services','service_timeline.total')->get();
+
+        return $timelines;
     }
 }
--- database/seeders/TimelineSeeder.php
+++ database/seeders/TimelineSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+
 use App\Models\Profile;
 use App\Models\Team;
 use App\Models\Timeline;
+use App\Models\Replacement;
+use App\Models\Supply;
+use App\Models\Service;
+use App\Models\Fail;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Faker\Generator;
@@ -48,5 +53,35 @@ class TimelineSeeder extends Seeder
             ]);
             $t->save;
         }
+
+$timelines = Timeline::all();
+foreach ($timelines as $t) {
+    $replacements = Replacement::inRandomOrder()->limit(rand(2, 6))->get()->pluck('id');
+    $t->replacements()->attach($replacements,["price"=>500,'quantity'=>3,'total'=>1500]);
+
+    $supplies = Supply::inRandomOrder()->limit(rand(2, 6))->get()->pluck('id');
+    $t->supplies()->attach($supplies,["price"=>350,'quantity'=>3,'total'=>1050]);
+
+    $services = Service::inRandomOrder()->limit(rand(2, 6))->get()->pluck('id');
+    $t->services()->attach($services,["price"=>6500,'total'=>6500]);
+}
+
+$fails = Fail::all();
+foreach ($fails as $t) {
+    $num = $faker->numberBetween($min = 3000, $max = 12000);
+    $q = $faker->numberBetween($min = 1, $max = 9);
+    $to=$num*$q;
+    $replacements = Replacement::inRandomOrder()->limit(rand(2, 6))->get()->pluck('id');
+    $t->replacements()->attach($replacements,["price"=>$num,'quantity'=>$q,'total'=>$to]);
+
+    $num = $faker->numberBetween($min = 1000, $max = 9000);
+    $q = $faker->numberBetween($min = 1, $max = 9);
+    $to=$num*$q;
+
+    $supplies = Supply::inRandomOrder()->limit(rand(2, 6))->get()->pluck('id');
+    $t->supplies()->attach($supplies,["price"=>$num,'quantity'=>$q,'total'=>$to]);
+    $num = $faker->numberBetween($min = 3600, $max = 13400);
+    $services = Service::inRandomOrder()->limit(rand(2, 6))->get()->pluck('id');
+    $t->services()->attach($services,["price"=>$num,'total'=>$num]);}
 
     }}

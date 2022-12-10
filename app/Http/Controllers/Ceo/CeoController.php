599d02c2af06ddc3907a26927769597c2136ad1d
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2022-12-10 07:38:19 -0400

    leccion 190

--- app/Http/Controllers/Ceo/CeoController.php
+++ app/Http/Controllers/Ceo/CeoController.php
@@ -10,9 +10,28 @@ class CeoController extends Controller
     public function index(DatosServiceInterface $datosServiceInterface)
     {
     $resp = $datosServiceInterface->gastosDeMantenimiento();
-    dd($resp);
+    foreach ($resp as $r){
+        $repuestos_mant[]=['name'=>'repuestos mantenimiento','y'=>$r->repuestos_mant];
+        $insumos_mant[]=['name'=>'insumos mantenimiento','y'=>$r->insumos_mant];
+        $servicios_mant[]=['name'=>'servicios mantenimiento','y'=>$r->servicios_mant];
+        $personal_mant[]=['name'=>'personal mantenimiento','y'=>$r->personal_mant];
+        $total_mant[]=['name'=>'gasto mantenimiento','y'=>$r->total_mant];
+    }
 
-return view('Ceo.index');
+
+
+    $fallas = $datosServiceInterface->gastosDeFallas();
+    foreach ($fallas as $r){
+        $repuestos_falla[]=['name'=>'repuestos de fallas','y'=>$r->repuestos_falla];
+        $insumos_falla[]=['name'=>'insumos de fallas','y'=>$r->insumos_falla];
+        $servicios_falla[]=['name'=>'servicios de fallas','y'=>$r->servicios_falla];
+        $personal_falla[]=['name'=>'personal de fallas','y'=>$r->personal_falla];
+        $total_falla[]=['name'=>'gasto de fallas','y'=>$r->total_falla];
+    }
+
+return view('Ceo.index',compact('repuestos_mant','insumos_mant','servicios_mant','personal_mant','total_mant',
+'repuestos_falla','insumos_falla','servicios_falla','personal_falla','total_falla'
+));
 
     }
 }
--- app/Services/DatosService.php
+++ app/Services/DatosService.php
@@ -2,6 +2,7 @@
 
 use App\Interfaces\DatosServiceInterface;
 use App\Models\Fail;
+use App\Models\Resume;
 use App\Models\Timeline;
 use Illuminate\Support\Facades\DB;
 
@@ -24,13 +25,18 @@ class DatosService implements DatosServiceInterface
 
     public function gastosDeMantenimiento(){
 
-        $gm=Timeline::select(DB::raw('DAY(start) as mes'),DB::raw('SUM(total_replacement) as repuestos_mant'), DB::raw('SUM(total_supply) as insumos_mant'),DB::raw('SUM(total_service) as servicios_mant'),DB::raw('SUM(total_workers) as personal_mant'),DB::raw('SUM(total) as costos_mant'))
+        $gm=Timeline::select(DB::raw('WEEK(start) as mes'),DB::raw('SUM(total_replacement) as repuestos_mant'), DB::raw('SUM(total_supply) as insumos_mant'),DB::raw('SUM(total_service) as servicios_mant'),DB::raw('SUM(total_workers) as personal_mant'),DB::raw('SUM(total) as costos_mant'))
+        ->groupBy('mes')->get();
+        return $gm;
+    }
+
+    public function gastosDeFallas(){
+
+        $gm=Resume::select(DB::raw('WEEK(reported_at) as mes'),DB::raw('SUM(total_replacement) as repuestos_mant'), DB::raw('SUM(total_supply) as insumos_mant'),DB::raw('SUM(total_service) as servicios_mant'),DB::raw('SUM(total_workers) as personal_mant'),DB::raw('SUM(total) as costos_mant'))
         ->groupBy('mes')->get();
         return $gm;
     }
 
 
-    public function gastosDeFallas(){
 
-    }
 }
